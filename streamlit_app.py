9f58b6119e3e3db1c48a3ef39f079ad8785bf2b4
Jay Kumar Lalwani <jk.lalwani78@gmail.com>
2025-09-02 15:33:53 +0200

    Update streamlit_app.py
    Add external API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,10 @@ if ingredients_list:
     
     time_to_insert = st.button('Submit Order')
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
